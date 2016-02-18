f3817f47408651fc4cd772506db375f5019fce86
Riley Rainey <riley@websimulations.com>
2016-02-18 10:47:51 -0600

    enhanced Mongo error reporting; test bundled frame

--- src/server/ecgsvc/index.js
+++ src/server/ecgsvc/index.js
@@ -27,73 +27,79 @@ app.oauth = oauthserver({
 app.all('/oauth/token', app.oauth.grant());
 
 app.post('/api/sampleframe', app.oauth.authorise(), function(req, res) {
-        var keys = [];
-        var data = req.body.data;
-        var toBeSaved = [];
-        var calls = [];
-        // 'data' array present?
-        if ( typeof data === 'undefined' || data === null ) {
-            res.json( { "keys": keys, "status": "missing data array in request" } );
-            return;
-        }
-        nasync.each(data, function(item, callback) {
-            var recordResponse = {};
-            // all fields present?
-            if (typeof item.id === 'string') {
-                
-                if (typeof item.datasetId === 'string' &&
-                     typeof item.date === 'string' &&
-                     typeof item.timestamp === 'number' &&
-                     typeof item.sampleCount === 'number' &&
-                     typeof item.samples === 'string') {
-                    
-                    var samples = new Buffer(item.samples, "base64");
-                    
-                    var a = new SampleFrame(
-                        {
-                            id: item.id,
-                            datasetId: item.datasetId,
-                            date: item.date,
-                            timestamp: item.timestamp,
-                            endTimestamp: item.endTimestamp,
-                            sampleCount: item.sampleCount,
-                            samples: samples
-                        });
-                    
-                    SampleFrame.findOne( { "id": a.id }, function(err,f) {
-                        if (f === null) {
-                            a.save( function(err) {
-                                recordResponse = { "id": a.id, "_id": a._id };
-                                keys.push( recordResponse );
-                                callback();
-                            });
-                        }
-                        else {
-                            recordResponse = { "id": a.id, "status": "duplicate" };
-                            keys.push( recordResponse );   
-                            callback();
-                        }
+    var keys = [];
+    var data = req.body.data;
+    var toBeSaved = [];
+    var calls = [];
+    // 'data' array present?
+    if ( typeof data === 'undefined' || data === null ) {
+        res.json( { "keys": keys, "status": "missing data array in request" } );
+        return;
+    }
+    nasync.each(data, function(item, callback) {
+        var recordResponse = {};
+        // all fields present?
+        if (typeof item.id === 'string') {
+
+            if (typeof item.datasetId === 'string' &&
+                 typeof item.date === 'string' &&
+                 typeof item.timestamp === 'number' &&
+                 typeof item.sampleCount === 'number' &&
+                 typeof item.samples === 'string') {
+
+                var samples = new Buffer(item.samples, "base64");
+
+                var a = new SampleFrame(
+                    {
+                        id: item.id,
+                        datasetId: item.datasetId,
+                        date: item.date,
+                        timestamp: item.timestamp,
+                        endTimestamp: item.endTimestamp,
+                        sampleCount: item.sampleCount,
+                        samples: samples
                     });
-                }
-                else {
-                    recordResponse = { "id": item.id, "status": "missing properties" };
-                    keys.push( recordResponse );
-                    callback();
-                }
+
+                SampleFrame.findOne( { "id": a.id }, function(err,f) {
+                    if (f === null) {
+                        a.save( function(err) {
+                            if (err) {
+                                recordResponse = { "id": a.id, "status": "error" };
+                                console.error("error on save of sample frame: " + err);
+                            }
+                            else {
+                                recordResponse = { "id": a.id, "_id": a._id };
+                            }
+                            keys.push( recordResponse );
+                            callback();
+                        });
+                    }
+                    else {
+                        recordResponse = { "id": a.id, "status": "duplicate" };
+                        keys.push( recordResponse );   
+                        callback();
+                    }
+                });
             }
             else {
-                recordResponse = { "id": "", "status": "missing properties" };
+                recordResponse = { "id": item.id, "status": "missing properties" };
                 keys.push( recordResponse );
                 callback();
             }
-        },
-        function(err) {
-            // All tasks are done now
-            res.json( { "keys": keys, "status": "ok" } );
-        });
-            
-        
+        }
+        else {
+            recordResponse = { "id": "", "status": "missing properties" };
+            keys.push( recordResponse );
+            callback();
+        }
+    },
+    function(err) {
+        // All tasks are done now
+        res.json( { "keys": keys, "status": "ok" } );
     });
+
+
+});
 
 
 app.get('/', app.oauth.authorise(), function (req, res) {
